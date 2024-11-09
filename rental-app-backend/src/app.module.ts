aa392749cd0908bda3e28876cdd867a35fbbe6f9
Conor Koritor <ckoritor@gmail.com>
2024-11-09 16:18:05 +0000

    Added code to connect to database and created Users Module, Controller, and Service

--- rental-app-backend/src/app.module.ts
+++ rental-app-backend/src/app.module.ts
@@ -4,11 +4,16 @@ dotenv.config();
 import { Module } from '@nestjs/common';
 import { PropertyModule } from './property/property.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     PropertyModule,
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      tls: true,
+      tlsCAFile: process.env.TLS_CA_FILE
+    }),
+    UsersModule,
   ]
 })
 export class AppModule {}
